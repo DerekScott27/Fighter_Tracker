c6063b0c65e0463b39a30e8de0eb1e26c18087db
DerekScott727 <derekschelberg@gmail.com>
2026-01-12 17:38:42 -0800

    Add session persistance in supabase.js

--- public/supabase.js
+++ public/supabase.js
@@ -3,4 +3,13 @@
 const SUPABASE_URL = 'https://rebompofijapbeohnfgd.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_b63PtZkkpUoL1nkqtIO4zg_r5y7EnUM';
 
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true
+    }
+  }
+);
